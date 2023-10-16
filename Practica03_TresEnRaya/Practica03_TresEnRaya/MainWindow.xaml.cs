48455545cd9dbc0096002c717bb2fbe31f44f07e
dam2b10 <dam2b10@.local>
2023-10-16 16:59:13 +0200

    Tres en raya empezao

---
+++ Practica03_TresEnRaya/Practica03_TresEnRaya/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practica03_TresEnRaya
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void EvtPonerFicha(object sender, RoutedEventArgs e)
+        {
+            int turno = 1;
+            Button btn = (Button)sender;
+            if (turno %2 ==0) {
+                btn.Content=
+            }
+            else if (turno %2 !=0) { 
+            
+            }
+        }
+    }
+}
